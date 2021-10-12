99050569cd157003064ba4c0f47abdc0239b8a02
SzymekN <nowak.szymek00@gmail.com>
2021-10-12 19:50:58 +0200

    refactorization

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import itertools
 import csv
 import sys
-from typing import Counter
 
 def count_points(word):
+    # count points for given word
     points = 0
     for letter in list(word):
         if letter == 'q' or letter == 'z':
@@ -21,21 +21,34 @@ def count_points(word):
         else:
             points += 1
     return points
+
+def variations(letters, words):
+    # generate all variations of all lengths in given list
+    solutions = []
+    for i in range(2,len(letters)+1):
+        test = ["".join(item) for item in itertools.permutations(letters, i)]
+        test = set(test)
+        for w in test:
+            if w in words:
+                solutions.append([w,count_points(w)])
+
+    print(f"All available solutions: {len(solutions)}")
+    return solutions
+
     
 maxInt = sys.maxsize
 
+# set field size as big as possible, dividing by 10 on overflow errors
 while True:
-    # decrease the maxInt value by factor 10 
-    # as long as the OverflowError occurs.
-
     try:
         csv.field_size_limit(maxInt)
         break
     except OverflowError:
         maxInt = int(maxInt/10)
 
-words = set()
 
+# create set containing every word in English
+words = set()
 with open('words.csv', encoding="utf-8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -46,30 +59,30 @@ with open('words.csv', encoding="utf-8") as csv_file:
             words.add(row[0].lower())
             line_count += 1
 
-letters = ['a','b','c','d','e','f','g','i']
-possibilities = []
+# game control on/off
+game_on = 'y'
+while game_on == 'y':
 
-for i in range(2,len(letters)+1):
-    test = ["".join(item) for item in itertools.permutations(letters, i)]
-    test = set(test)
-    for w in test:
-        if w in words:
-            possibilities.append([w,count_points(w)])
+    # input letters
+    letters = list(input("Enter letters: ").replace(" ",""))
+    print(f"Entered letters: {' '.join(letters)}")
 
-print(f"All available solutions: {len(possibilities)}")
-possibilities.sort(key= lambda x: x[1], reverse= True)
+    # generate solutions and sort 
+    possibilities = variations(letters, words)
+    possibilities.sort(key= lambda x: x[1], reverse= True)
 
-best = 10
-if len(possibilities) < best:
-    best = len(possibilities)
+    best = min(len(possibilities),10)
+    print(f"Best {best} solutions")
 
-print(f"Best {best} solutions")
-counter = 0
-for pos in possibilities:
-    choice = 'y'
-    print(f'{pos[0]} - {pos[1]}pt.')
-    counter += 1
-    if counter % 10 == 0:
-        choice = input("Show next 10 solutions? (y/n)")
-    if choice == 'n':
-        break
+    # print possible solutions
+    counter = 0
+    for pos in possibilities:
+        choice = 'y'
+        print(f'{pos[0]} - {pos[1]}pt.')
+        counter += 1
+        if counter % 10 == 0:
+            choice = input("Show next 10 solutions? (y/n): ")
+        if choice == 'n':
+            break
+
+    game_on = input("Continue game? (y/n): ")
